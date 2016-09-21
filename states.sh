d72eb4531a13c42bb1a7a096a5da518b1649ddb4
Evgeniy Bekhterev <jbe@mail.ru>
2016-09-21 16:52:46 +0300

    Create states.sh
    Reset states if GW went off and returned back

---
+++ states.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ 
+#Auto recover script VER 1.0
+ 
+reset_voip_states() {
+   echo "Reset states"
+   /sbin/pfctl -i vmx0 -k 192.168.254.11
+}
+
+gwStat=`/root/gw.php | grep WAN2 | awk -F'[:]' '{print $5}'`
+
+if [ "$gwStat" == "Online" ]
+    then
+        echo "WAN2 GW is ONLINE!"
+        test -f ~/off.lbl && reset_voip_states && rm ~/off.lbl || echo "No mark, everything is ok"
+    else
+        echo "WAN2 GW is OFFLINE!"
+        touch ~/off.lbl
+fi
