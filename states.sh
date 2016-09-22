8e14cb4c878ad83cf2483adce7a6e1135b081e64
Evgeniy Bekhterev <jbe@mail.ru>
2016-09-22 12:45:33 +0300

    Comments added

--- states.sh
+++ states.sh
@@ -15,27 +15,36 @@ voipNet="192.168.185.254"
 
 CURDATE=`date "+%Y%m%d:%H%M"`
 
+# Function for resetting states. 
+# Enlists all state on $nicName interface for $voipNet and saves in states.list
 reset_voip_states() {
     echo "$CURDATE:Resetting states"
+    # Get states
     /sbin/pfctl -i $nicName -ss -vv | grep $voipNet > states.list
 
     /bin/cat /root/states/states.list | while read -r line; do
+        # get source of state 
         src=`echo $line | awk -F'[ ]' '{print $3}' | awk -F'[:]' '{print $1}'`
+        # get destination of state
         dst=`echo $line | awk -F'[ ]' '{print $6}' | awk -F'[:]' '{print $1}'`
         echo "$CURDATE:pfctl -i $nicName -k $src -k $dst"
+        # kill state from $src to $dst on $nicName
         /sbin/pfctl -i $nicName -k $src -k $dst
     done
+    # remove states list
     rm states.list
 }
 
-
+# check gwName state
 gwStat=`/root/states/gw.php | grep $gwName | awk -F'[:]' '{print $5}'`
 
 if [ "$gwStat" == "Online" ]
     then
         echo "$CURDATE:$gwName is ONLINE!"
+        # if gwName is online, we need check if it was offline or not, if was - start reset_voip_states function and remove label
         test -f ~/states/off.lbl && reset_voip_states && rm ~/states/off.lbl || echo "$CURDATE:No mark, everything is ok"
     else
         echo "$CURDATE:$gwName is OFFLINE!"
+        # create label that gwName was offline
         touch ~/states/off.lbl
 fi
